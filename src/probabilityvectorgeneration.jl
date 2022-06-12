77e88a97aa59b99fb39ea96ba2d2baf63f9c3226
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-12 08:03:16 -0700

    Basic PVG definitions

--- src/probabilityvectorgeneration.jl
+++ src/probabilityvectorgeneration.jl
@@ -19,3 +19,48 @@
 # from state to state, whereas in practice, one may wish to re-use a partial
 # state from an earlier step, so that a simple composition such as f ∘ g ∘ h
 # would fail.
+
+# The result of the function applied to each element of each element of A
+# should always be Tuple{Vector{Int}, Vector{<:AbstractFloat}}
+
+function pvg(f::Function, A::AbstractArray{Vector{T}, N}, ws) where {T, N}
+    map(a -> map(x -> f(x, ws), a), A)
+end
+
+function pvg!(f::Function, B::AbstractArray{Vector{Tuple{Vector{Int}, Vector{S}}}, N}, A::AbstractArray{Vector{T}, N}, ws) where {T, N} where {S<:AbstractFloat}
+    for i ∈ eachindex(B, A)
+        B[i] = map(x -> f(x, ws), A[i])
+    end
+    B
+end
+
+function pvg(f::Function, A::AbstractArray{Vector{T}, N}, ws::Tuple{}) where {T, N}
+    map(a -> map(f, a), A)
+end
+
+function pvg!(f::Function, B::AbstractArray{Vector{Tuple{Vector{Int}, Vector{S}}}, N}, A::AbstractArray{Vector{T}, N}, ws::Tuple{}) where {T, N} where {S<:AbstractFloat}
+    for i ∈ eachindex(B, A)
+        B[i] = map(f, A[i])
+    end
+    B
+end
+
+# A simplification: an array of T, rather than Vector{T}
+
+pvg(f::Function, A::AbstractArray{T, N}, ws) where {T, N} = map(x -> f(x, ws), A)
+
+function pvg!(f::Function, B::AbstractArray{Tuple{Vector{Int}, Vector{S}}, N}, A::AbstractArray{T, N}, ws) where {T, N} where {S<:AbstractFloat}
+    for i ∈ eachindex(B, A)
+        B[i] = f(A[i], ws)
+    end
+    B
+end
+
+pvg(f::Function, A::AbstractArray{T, N}, ws::Tuple{}) where {T, N} = map(f, A)
+
+function pvg!(f::Function, B::AbstractArray{Tuple{Vector{Int}, Vector{S}}, N}, A::AbstractArray{T, N}, ws::Tuple{}) where {T, N} where {S<:AbstractFloat}
+    for i ∈ eachindex(B, A)
+        B[i] = f(A[i])
+    end
+    B
+end
