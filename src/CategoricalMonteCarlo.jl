eecb126e9803ac91bdf5d30098dc76c344980c63
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-06-12 06:46:56 -0700

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.28

---
+++ src/CategoricalMonteCarlo.jl
@@ -0,0 +1,5 @@
+module CategoricalMonteCarlo
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using CategoricalMonteCarlo
+using Test
+
+@testset "CategoricalMonteCarlo.jl" begin
+    # Write your tests here.
+end
