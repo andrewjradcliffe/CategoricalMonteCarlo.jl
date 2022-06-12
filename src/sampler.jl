75c02a26d095ab41f31bd5be4015925032e7152f
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-12 16:35:52 -0700

    Change samplers over to more performant memory traversal

--- src/sampler.jl
+++ src/sampler.jl
@@ -20,22 +20,29 @@
 # The expected case: vectors of sparse vectors (as their bare components)
 function sample(::Type{S}, A::AbstractArray{Vector{Tuple{Vector{Int}, Vector{T}}}, N}, num_samples::Int, num_categories::Int, dims::NTuple{P, Int}) where {S<:Real} where {P} where {T<:AbstractFloat, N}
     Dᴬ = size(A)
-    Dᴮ = tuple(num_categories, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))..., num_samples)
+    Dᴮ = tuple(num_categories, num_samples, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))...)
     B = similar(A, S, Dᴮ)
     fill!(B, zero(S))
     sample!(B, A, dims)
 end
 
 function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{Tuple{Vector{Int}, Vector{T}}}, N}, dims::NTuple{P, Int}) where {S<:Real, N′} where {P} where {T<:AbstractFloat, N}
-    keeps = ntuple(d -> d ∉ dims, Val(N))
-    defaults = ntuple(d -> firstindex(A, d), Val(N))
-    for j ∈ axes(B, N′)
-        for IA ∈ CartesianIndices(A)
-            IR = Broadcast.newindex(IA, keeps, defaults)
-            a = A[IA]
-            for (Iₛ, ω) ∈ a
-                c = categorical(ω)
-                B[Iₛ[c], IR, j] += one(S)
+    # keep = ntuple(d -> d ∉ dims, Val(N))
+    # default = ntuple(d -> firstindex(A, d), Val(N))
+    keep, default = Broadcast.shapeindexer(axes(B)[3:end])
+    C = Vector{Int}(undef, size(B, 2))
+    U = Vector{Float64}(undef, size(B, 2))
+    Σω = Vector{T}()
+    for IA ∈ CartesianIndices(A)
+        IR = Broadcast.newindex(IA, keep, default)
+        a = A[IA]
+        for (Iₛ, ω) ∈ a
+            @inline resize!(Σω, length(ω))
+            @inline cumsum!(Σω, ω)
+            categorical!(C, U, Σω)
+            for j ∈ axes(B, 2)
+                c = C[j]
+                B[Iₛ[c], j, IR] += one(S)
             end
         end
     end
@@ -45,26 +52,32 @@ end
 # A simplification: an array of sparse vectors
 function sample(::Type{S}, A::AbstractArray{Tuple{Vector{Int}, Vector{T}}, N}, num_samples::Int, num_categories::Int, dims::NTuple{P, Int}) where {S<:Real} where {P} where {T<:AbstractFloat, N}
     Dᴬ = size(A)
-    Dᴮ = tuple(num_categories, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))..., num_samples)
+    Dᴮ = tuple(num_categories, num_samples, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))...)
     B = similar(A, S, Dᴮ)
     fill!(B, zero(S))
     sample!(B, A, dims)
 end
 
 function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Tuple{Vector{Int}, Vector{T}}, N}, dims::NTuple{P, Int}) where {S<:Real, N′} where {P} where {T<:AbstractFloat, N}
-    keeps = ntuple(d -> d ∉ dims, Val(N))
-    defaults = ntuple(d -> firstindex(A, d), Val(N))
-    for j ∈ axes(B, N′)
-        for IA ∈ CartesianIndices(A)
-            IR = Broadcast.newindex(IA, keeps, defaults)
-            Iₛ, ω = A[IA]
-            c = categorical(ω)
-            B[Iₛ[c], IR, j] += one(S)
+    # keep = ntuple(d -> d ∉ dims, Val(N))
+    # default = ntuple(d -> firstindex(A, d), Val(N))
+    keep, default = Broadcast.shapeindexer(axes(B)[3:end])
+    C = Vector{Int}(undef, size(B, 2))
+    U = Vector{Float64}(undef, size(B, 2))
+    Σω = Vector{T}()
+    for IA ∈ CartesianIndices(A)
+        IR = Broadcast.newindex(IA, keep, default)
+        Iₛ, ω = A[IA]
+        @inline resize!(Σω, length(ω))
+        @inline cumsum!(Σω, ω)
+        categorical!(C, U, Σω)
+        for j ∈ axes(B, 2)
+            c = C[j]
+            B[Iₛ[c], j, IR] += one(S)
         end
     end
     B
 end
-
 
 # # A simple check
 # A = rand(2,3,4,5,6);
@@ -73,35 +86,38 @@ end
 # num_categories = 3
 # num_samples = 10
 
-# keeps = ntuple(d -> d ∉ dims, Val(N))
-# defaults = ntuple(d -> firstindex(A, d), Val(N))
+# keep = ntuple(d -> d ∉ dims, Val(N))
+# default = ntuple(d -> firstindex(A, d), Val(N))
 
 # Dᴬ = size(A)
 # Dᴮ = tuple(num_categories, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))..., num_samples)
 # B = similar(A, S, Dᴮ);
-# keeps_b, defaults_b = Broadcast.newindexer(B)
-# keeps_b[2:end-1] == keeps
+# keep_b, default_b = Broadcast.newindexer(B)
+# keep_b[2:end-1] == keep
 
 # Specialized method for eltype(A)::Vector{Vector{Int}}
 # or, in other words, where the probability mass on each element is 1 / length(Iₛ)
 function sample(::Type{S}, A::AbstractArray{Vector{Vector{Int}}, N}, num_samples::Int, num_categories::Int, dims::NTuple{P, Int}) where {S<:Real} where {P} where {N}
     Dᴬ = size(A)
-    Dᴮ = tuple(num_categories, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))..., num_samples)
+    Dᴮ = tuple(num_categories, num_samples, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))...)
     B = similar(A, S, Dᴮ)
     fill!(B, zero(S))
     sample!(B, A, dims)
 end
 
 function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{Vector{Int}}, N}, dims::NTuple{P, Int}) where {S<:Real, N′} where {P} where {N}
-    keeps = ntuple(d -> d ∉ dims, Val(N))
-    defaults = ntuple(d -> firstindex(A, d), Val(N))
-    for j ∈ axes(B, N′)
-        for IA ∈ CartesianIndices(A)
-            IR = Broadcast.newindex(IA, keeps, defaults)
-            a = A[IA]
-            for Iₛ ∈ a
-                c = rand(Iₛ)
-                B[c, IR, j] += one(S)
+    # keep = ntuple(d -> d ∉ dims, Val(N))
+    # default = ntuple(d -> firstindex(A, d), Val(N))
+    keep, default = Broadcast.shapeindexer(axes(B)[3:end])
+    C = Vector{Int}(undef, size(B, 2))
+    for IA ∈ CartesianIndices(A)
+        IR = Broadcast.newindex(IA, keep, default)
+        a = A[IA]
+        for Iₛ ∈ a
+            rand!(C, Iₛ)
+            for j ∈ axes(B, 2)
+                c = C[j]
+                B[c, j, IR] += one(S)
             end
         end
     end
@@ -118,12 +134,14 @@ function sample(::Type{S}, A::AbstractArray{Vector{Int}, N}, num_samples::Int, n
 end
 
 function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{Int}, N}, dims::NTuple{P, Int}) where {S<:Real, N′} where {P} where {N}
-    keeps = ntuple(d -> d ∉ dims, Val(N))
-    defaults = ntuple(d -> firstindex(A, d), Val(N))
-    for j ∈ axes(B, N′)
-        for IA ∈ CartesianIndices(A)
-            IR = Broadcast.newindex(IA, keeps, defaults)
-            Iₛ = A[IA]
+    # keep = ntuple(d -> d ∉ dims, Val(N))
+    # default = ntuple(d -> firstindex(A, d), Val(N))
+    keep, default = Broadcast.shapeindexer(axes(B)[3:end])
+    C = Vector{Int}(undef, size(B, 2))
+    for IA ∈ CartesianIndices(A)
+        IR = Broadcast.newindex(IA, keep, default)
+        Iₛ = A[IA]
+        for j ∈ axes(B, 2)
             c = rand(Iₛ)
             B[c, IR, j] += one(S)
         end
@@ -149,11 +167,11 @@ function tsample(::Type{S}, A::AbstractArray{Vector{Vector{Int}}, N}, num_sample
 end
 
 function tsample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{Vector{Int}}, N}, dims::NTuple{P, Int}) where {S<:Real, N′} where {P} where {N}
-    keeps = ntuple(d -> d ∉ dims, Val(N))
-    defaults = ntuple(d -> firstindex(A, d), Val(N))
+    keep = ntuple(d -> d ∉ dims, Val(N))
+    default = ntuple(d -> firstindex(A, d), Val(N))
     Threads.@threads for j ∈ axes(B, N′)
         for IA ∈ CartesianIndices(A)
-            IR = Broadcast.newindex(IA, keeps, defaults)
+            IR = Broadcast.newindex(IA, keep, default)
             a = A[IA]
             for Iₛ ∈ a
                 c = rand(Iₛ)
