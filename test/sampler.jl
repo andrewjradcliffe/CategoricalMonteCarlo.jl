462f16d4b91c49ad4eb279e11e1a74469da96984
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-13 11:22:54 -0700

    squash! Sampler tests

--- test/sampler.jl
+++ test/sampler.jl
@@ -1,7 +1,13 @@
 # Tests of sampler functionality
 
 @testset "sampler, equal probability mass" begin
+    # # Specialized method for eltype(A)::Vector{Vector{Int}}
     A = [[[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]]
+    B = sample(Int, A, 10, num_cat(A), (1,))
+    @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+    @test all(minimum(B, dims=2) .≥ 0)
+    # # A simplification: an array of sparse vectors
+    A = [[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]
     B = sample(Int, A, 10, num_cat(A), (1,))
     @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
     @test all(minimum(B, dims=2) .≥ 0)
