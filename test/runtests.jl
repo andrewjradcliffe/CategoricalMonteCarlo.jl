5b953002e0c024a783685f8c8382542a083e0559
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-13 11:20:57 -0700

    Iterated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using CategoricalMonteCarlo
 using Test
 
-@testset "CategoricalMonteCarlo.jl" begin
-    # Write your tests here.
+# @testset "CategoricalMonteCarlo.jl" begin
+# end
+const tests = [
+    "sampler.jl",
+    # "tsampler.jl",
+]
+
+for t in tests
+    @testset "Test $t" begin
+        include(t)
+    end
 end
+
