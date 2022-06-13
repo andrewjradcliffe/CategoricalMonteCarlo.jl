39952ae4774923e088892c801660fcc44388ae3b
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-13 11:39:32 -0700

    Sweep tests

--- test/sampler.jl
+++ test/sampler.jl
@@ -1,14 +1,41 @@
 # Tests of sampler functionality
 
 @testset "sampler, equal probability mass" begin
-    # # Specialized method for eltype(A)::Vector{Vector{Int}}
-    A = [[[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]]
-    B = sample(Int, A, 10, num_cat(A), (1,))
-    @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
-    @test all(minimum(B, dims=2) .≥ 0)
-    # # A simplification: an array of sparse vectors
-    A = [[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]
-    B = sample(Int, A, 10, num_cat(A), (1,))
-    @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
-    @test all(minimum(B, dims=2) .≥ 0)
+    for region ∈ [1, 2, 3, 4, 5, (1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (1,2,3), (1,2,4), (2,3,4), (1,2,3,4), :]
+        for i = 1:15
+            for j = -1:1
+                n_sim = (1 << i) + j
+                # # Specialized method for eltype(A)::Vector{Vector{Int}}
+                A = [[[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]]
+                B = sample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+                # # A simplification: an array of sparse vectors
+                A = [[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]
+                B = sample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+            end
+        end
+    end
+end
+
+@testset "sampler, unequal probability mass" begin
+    for region ∈ [1, 2, 3, 4, 5, (1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (1,2,3), (1,2,4), (2,3,4), (1,2,3,4), :]
+        for i = 1:15
+            for j = -1:1
+                n_sim = (1 << i) + j
+                # # Specialized method for eltype(A)::Vector{Vector{Int}}
+                A = [[([1, 2], [0.3, 0.7]), ([1,2,3,4], [0.2, 0.3, 0.4, 0.1]), ([1,2,3,4,5,6], [0.1, 0.1, 0.1, 0.1,0.1, 0.5])]]
+                B = sample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+                # # A simplification: an array of sparse vectors
+                A = [([1, 2], [0.3, 0.7]), ([1,2,3,4], [0.2, 0.3, 0.4, 0.1]), ([1,2,3,4,5,6], [0.1, 0.1, 0.1, 0.1,0.1, 0.5])]
+                B = sample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+            end
+        end
+    end
 end
