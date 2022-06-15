590cd37b11debae470697818091bad47f4ab07fb
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-15 11:57:53 -0700

    Add doctest

--- src/normalizations.jl
+++ src/normalizations.jl
@@ -11,10 +11,37 @@ _typeofinv(x) = typeof(inv(x))
     normalize1!(A::AbstractArray{<:Real})
 
 Normalize the values in `A` such that `sum(A) ≈ 1` and `0 ≤ A[i] ≤ 1` ∀i.
-It is assumed that `A[i] ≥ 0 ∀i`. This is not quite the L¹-norm, which would
-require that `abs(A[i])` be used.
+This is not quite the L¹-norm, which would require that `abs(A[i])` be used.
+It is assumed that `0 ≤ A[i] < Inf` ∀i. `Inf` values are not handled and
+will result in `NaN`'s.
 
 See also: [`normalize1`](@ref)
+
+```jldoctest
+julia> normalize1!([1.0, 2.0, 3.0])
+3-element Vector{Float64}:
+ 0.16666666666666666
+ 0.3333333333333333
+ 0.5
+
+julia> normalize1!([1.0, 2.0, Inf])
+3-element Vector{Float64}:
+   0.0
+   0.0
+ NaN
+
+julia> normalize1!([1.0, 2.0, NaN])    # NaN propagates, as expected
+3-element Vector{Float64}:
+ NaN
+ NaN
+ NaN
+
+julia> normalize1!([1.0, -2.0, 3.0])    # not the L¹-norm
+3-element Vector{Float64}:
+  0.5
+ -1.0
+  1.5
+```
 """
 function normalize1!(A::AbstractArray{T}) where {T<:Real}
     s = zero(T)
@@ -54,7 +81,7 @@ It is assumed that `A[i] ≥ 0` ∀i.
 
 See also: [`normalize1!`](@ref)
 """
-normalize1(A::AbstractArray{T<:Real}) = normalize1!(similar(A, _typeofinv(first(A))), A)
+normalize1(A::AbstractArray{<:Real}) = normalize1!(similar(A, _typeofinv(first(A))), A)
 
 ################
 @noinline function _check_normweights(I::Vector{Int}, x)
