88a4bc1b77b37e7a456fb4d142b119172676cd2c
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-16 10:48:46 -0700

    Extra SparseVector tests

--- test/sampler.jl
+++ test/sampler.jl
@@ -417,6 +417,31 @@ end
         B = Matrix{Union{Int16,Int32}}(undef, 6, 10)
         @test_throws MethodError sample!(B, A)
     end
+    # extra SparseVector tests
+    n_sim = 10
+    sv1 = SparseVector(10, [1, 10], [0.3, 0.7])
+    sv2 = SparseVector(4, [1,2,3,4], [0.2, 0.3, 0.4, 0.1])
+    sv3 = SparseVector(10, [2, 4, 6, 8, 9, 10], [0.1, 0.1, 0.1, 0.1,0.5, 0.1])
+    A = [[sv1, sv2, sv3]]
+    B = sample(Int, A, n_sim)
+    @test all(maximum(B, dims=2) .≤ [2, 2, 1, 2, 0, 1, 0, 1, 1, 2])
+    @test all(≥(0), minimum(B, dims=2))
+    @test all(==(3), sum(B, dims=1))
+    @test all(sum(B, dims=2) .≤ n_sim .* [2, 2, 1, 2, 0, 1, 0, 1, 1, 2])
+    A = [sv1, sv2, sv3]
+    B = sample(Int, A, n_sim)
+    @test all(maximum(B, dims=2) .≤ [2, 2, 1, 2, 0, 1, 0, 1, 1, 2])
+    @test all(≥(0), minimum(B, dims=2))
+    @test all(==(3), sum(B, dims=1))
+    @test all(sum(B, dims=2) .≤ n_sim .* [2, 2, 1, 2, 0, 1, 0, 1, 1, 2])
+    B = sample(Int, sv1, n_sim)
+    @test all(==(1), sum(B, dims=1))
+    sample!(B, sv2)
+    @test all(==(2), sum(B, dims=1))
+    sample!(B, sv3)
+    @test all(==(3), sum(B, dims=1))
+    @test all(maximum(B, dims=2) .≤ [2, 2, 1, 2, 0, 1, 0, 1, 1, 2])
+    @test all(≥(0), minimum(B, dims=2))
 end
 
 
