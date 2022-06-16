e8c1e906eefc6fd6de02ac6fd0d2dde2740b25f0
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-15 19:21:19 -0700

    Tests of dense tsampler

--- test/tsampler.jl
+++ test/tsampler.jl
@@ -104,7 +104,7 @@
     end
 end
 
-@testset "tsampler, unequal probability mass" begin
+@testset "sparse tsampler, unequal probability mass" begin
     for region ∈ [1, 2, 3, 4, 5, (1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (1,2,3), (1,2,4), (2,3,4), (1,2,3,4), :, (),  (1, 5), (2, 5), (1,2,5), (5,6,7)]
         for i = 1:15
             for j = -1:1
@@ -158,14 +158,14 @@ end
             @test all(==(3), sum(B, dims=(1,3)))
             @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
             @test_throws MethodError tsample(Complex{T}, A, n_sim)
-            A = [1,2,3,4,5,6]
+            A = ([1,2,3,4,5,6], [0.1, 0.1, 0.1, 0.1,0.1, 0.5])
             B = @inferred tsample(T, A, n_sim)
             @test all(==(1), sum(B, dims=1))
-            A = [1,2,3,4]
+            A = ([1,2,3,4], [0.2, 0.3, 0.4, 0.1])
             tsample!(B, A)
             @test all(==(2), sum(B, dims=1))
             @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
-            A = [1,2]
+            A = ([1, 2], [0.3, 0.7])
             tsample!(B, A)
             @test all(==(3), sum(B, dims=1))
             @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
@@ -207,6 +207,111 @@ end
         @test_throws MethodError tsample!(B, A)
     end
 end
+
+@testset "dense tsampler, (un)equal probability mass" begin
+    for region ∈ [1, 2, 3, 4, 5, (1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (1,2,3), (1,2,4), (2,3,4), (1,2,3,4), :, (),  (1, 5), (2, 5), (1,2,5), (5,6,7)]
+        for i = 1:15
+            for j = -1:1
+                n_sim = (1 << i) + j
+                # # Specialized method for eltype(A)::Vector{Vector{Int}}
+                A = [[[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]]
+                B = tsample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+                @test all(==(3), sum(B, dims=1))
+                @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+                # # A simplification: an array of sparse vectors
+                A = [[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]
+                B = tsample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+                @test all(==(3), sum(B, dims=(1,3)))
+                @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+                # # The simplest case: a sparse vector
+                A = [0.1, 0.1, 0.1, 0.1,0.1, 0.5]
+                B = tsample(Int, A, n_sim, dims=region)
+                @test all(==(1), sum(B, dims=1))
+                A = [0.2, 0.3, 0.4, 0.1]
+                tsample!(B, A)
+                @test all(==(2), sum(B, dims=1))
+                @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
+                A = [0.3, 0.7]
+                tsample!(B, A)
+                @test all(==(3), sum(B, dims=1))
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(≥(0), minimum(B, dims=2))
+            end
+        end
+    end
+    @testset "eltypes" begin
+        n_sim = 10
+        # Types one would normally expect
+        for T ∈ [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128,
+                 Float16, Float32, Float64, BigFloat, BigInt, Rational]
+            A = [[[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]]
+            B = @inferred tsample(T, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=1))
+            @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            @test_throws MethodError tsample(Complex{T}, A, n_sim)
+            A = [[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]
+            B = @inferred tsample(T, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=(1,3)))
+            @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            @test_throws MethodError tsample(Complex{T}, A, n_sim)
+            A = [0.1, 0.1, 0.1, 0.1,0.1, 0.5]
+            B = @inferred tsample(T, A, n_sim)
+            @test all(==(1), sum(B, dims=1))
+            A = [0.2, 0.3, 0.4, 0.1]
+            tsample!(B, A)
+            @test all(==(2), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
+            A = [0.3, 0.7]
+            tsample!(B, A)
+            @test all(==(3), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test_throws MethodError tsample(Complex{T}, A, n_sim)
+        end
+        # Composite numeric types
+        for T ∈ [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128]
+            A = [[[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]]
+            B = @inferred tsample(Rational{T}, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=1))
+            @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            A = [[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]
+            B = @inferred tsample(Rational{T}, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=(1,3)))
+            @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            A = [0.1, 0.1, 0.1, 0.1,0.1, 0.5]
+            B = @inferred tsample(Rational{T}, A, n_sim)
+            @test all(==(1), sum(B, dims=1))
+            A = [0.2, 0.3, 0.4, 0.1]
+            tsample!(B, A)
+            @test all(==(2), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
+            A = [0.3, 0.7]
+            tsample!(B, A)
+            @test all(==(3), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+        end
+        # Real, AbstractFloat, Integer, Signed, Unsigned. work but should be avoided
+        A = [[[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.5, 0.2, 0.2, 0.05,0.025, 0.025]]] # slight change to increase probability of Inexact throw
+        @test_throws InexactError tsample(Bool, A, 1000)
+        @test_throws MethodError tsample(Union{Int16, Int32}, A, n_sim)
+        B = Matrix{Union{Int16,Int32}}(undef, 6, 10)
+        @test_throws MethodError tsample!(B, A)
+    end
+end
+
 
 @testset "tsampler inferface throws" begin
     n_sim = 10
