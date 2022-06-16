8aee2d6735d29b189c09f3a5d28220908d9bd0f5
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-16 10:20:46 -0700

    SparseVector tsampler tests

--- test/tsampler.jl
+++ test/tsampler.jl
@@ -5,7 +5,7 @@
         for i = 1:15
             for j = -1:1
                 n_sim = (1 << i) + j
-                # # Specialized method for eltype(A)::Vector{Vector{Int}}
+                # # Specialized method for eltype(A)::Array{Vector{Int}}
                 A = [[[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]]
                 B = tsample(Int, A, n_sim, num_cat(A), dims=region)
                 @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
@@ -109,7 +109,7 @@ end
         for i = 1:15
             for j = -1:1
                 n_sim = (1 << i) + j
-                # # Specialized method for eltype(A)::Vector{Vector{Int}}
+                # # Specialized method for eltype(A)::Array{Vector{Int}}
                 A = [[([1, 2], [0.3, 0.7]), ([1,2,3,4], [0.2, 0.3, 0.4, 0.1]), ([1,2,3,4,5,6], [0.1, 0.1, 0.1, 0.1,0.1, 0.5])]]
                 B = tsample(Int, A, n_sim, num_cat(A), dims=region)
                 @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
@@ -213,21 +213,21 @@ end
         for i = 1:15
             for j = -1:1
                 n_sim = (1 << i) + j
-                # # Specialized method for eltype(A)::Vector{Vector{Int}}
+                # # Specialized method for eltype(A)::Array{Vector{<:AbstractFloat}}
                 A = [[[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]]
                 B = tsample(Int, A, n_sim, num_cat(A), dims=region)
                 @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
                 @test all(minimum(B, dims=2) .≥ 0)
                 @test all(==(3), sum(B, dims=1))
                 @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
-                # # A simplification: an array of sparse vectors
+                # # A simplification: an array of dense vectors
                 A = [[0.3, 0.7], [0.2, 0.3, 0.4, 0.1], [0.1, 0.1, 0.1, 0.1,0.1, 0.5]]
                 B = tsample(Int, A, n_sim, num_cat(A), dims=region)
                 @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
                 @test all(minimum(B, dims=2) .≥ 0)
                 @test all(==(3), sum(B, dims=(1,3)))
                 @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
-                # # The simplest case: a sparse vector
+                # # The simplest case: a dense vector
                 A = [0.1, 0.1, 0.1, 0.1,0.1, 0.5]
                 B = tsample(Int, A, n_sim, dims=region)
                 @test all(==(1), sum(B, dims=1))
@@ -312,30 +312,109 @@ end
     end
 end
 
-
-@testset "tsampler inferface throws" begin
-    n_sim = 10
-    A = [1,2,3,4,5,6]
-    @test_throws MethodError tsample(Int, A, n_sim, dims=1:2)
-    @test_throws MethodError tsample(Int, A, n_sim, dims=[1,2,3])
-    @test_throws MethodError tsample(Int, A, n_sim, dims=[.1 .2; .3 .4])
-    A = ([1,2,3,4,5,6], [0.1, 0.1, 0.1, 0.1,0.1, 0.5])
-    @test_throws MethodError tsample(Int, A, n_sim, dims=1:2)
-    @test_throws MethodError tsample(Int, A, n_sim, dims=[1,2,3])
-    @test_throws MethodError tsample(Int, A, n_sim, dims=[.1 .2; .3 .4])
-end
-
-@testset "tsampler, equal probability mass" begin
-    n_sim = 100
-    A = [[1, 2], [1, 2, 3, 4], [1, 2, 3, 4, 5, 6]]
-    D = fill(A, 10,5,5);
-    # # Specialized method for eltype(A)::Vector{Vector{Int}}
-    # Admittedly, not very meaningful test as
-    Pr = 1/2 * 1/4 * 1/6
-    lPr = length(D) * log(Pr) # necessary to even view as log probability
-    lPr * log10(ℯ) # or on log10 scale
-    B = tsample(Int, D, n_sim, num_cat(D), dims=(1,2,3))
-    @test all(maximum(B, dims=2) .≤  length(D) .* [3; 3; 2; 2; 1; 1])
-    @test all(minimum(B, dims=2) .≥ 0)
-    @test all(==(length(A) * length(D)), sum(B, dims=1))
+@testset "SparseVector tsampler, unequal probability mass" begin
+    sv1 = SparseVector(2, [1,2], [0.3, 0.7])
+    sv2 = SparseVector(4, [1,2,3,4], [0.2, 0.3, 0.4, 0.1])
+    sv3 = SparseVector(6, [1,2,3,4,5,6], [0.1, 0.1, 0.1, 0.1,0.1, 0.5])
+    for region ∈ [1, 2, 3, 4, 5, (1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (1,2,3), (1,2,4), (2,3,4), (1,2,3,4), :, (),  (1, 5), (2, 5), (1,2,5), (5,6,7)]
+        for i = 1:15
+            for j = -1:1
+                n_sim = (1 << i) + j
+                # # Specialized method for eltype(A)::Array{SparseVector{<:AbstractFloat}}
+                A = [[sv1, sv2, sv3]]
+                B = tsample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+                @test all(==(3), sum(B, dims=1))
+                @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+                # # A simplification: an array of SparseVector
+                A = [sv1, sv2, sv3]
+                B = tsample(Int, A, n_sim, num_cat(A), dims=region)
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(minimum(B, dims=2) .≥ 0)
+                @test all(==(3), sum(B, dims=(1,3)))
+                @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+                # # The simplest case: a SparseVector
+                A = sv3
+                B = tsample(Int, A, n_sim, dims=region)
+                @test all(==(1), sum(B, dims=1))
+                A = sv2
+                tsample!(B, A)
+                @test all(==(2), sum(B, dims=1))
+                @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
+                A = sv1
+                tsample!(B, A)
+                @test all(==(3), sum(B, dims=1))
+                @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+                @test all(≥(0), minimum(B, dims=2))
+            end
+        end
+    end
+    @testset "eltypes" begin
+        n_sim = 10
+        # Types one would normally expect
+        for T ∈ [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128,
+                 Float16, Float32, Float64, BigFloat, BigInt, Rational]
+            A = [[sv1, sv2, sv3]]
+            B = @inferred tsample(T, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=1))
+            @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            @test_throws MethodError tsample(Complex{T}, A, n_sim)
+            A = [sv1, sv2, sv3]
+            B = @inferred tsample(T, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=(1,3)))
+            @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            @test_throws MethodError tsample(Complex{T}, A, n_sim)
+            A = sv3
+            B = @inferred tsample(T, A, n_sim)
+            @test all(==(1), sum(B, dims=1))
+            A = sv2
+            tsample!(B, A)
+            @test all(==(2), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
+            A = sv1
+            tsample!(B, A)
+            @test all(==(3), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test_throws MethodError tsample(Complex{T}, A, n_sim)
+        end
+        # Composite numeric types
+        for T ∈ [Int8, Int16, Int32, Int64, Int128, UInt8, UInt16, UInt32, UInt64, UInt128]
+            A = [[sv1, sv2, sv3]]
+            B = @inferred tsample(Rational{T}, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=1))
+            @test all(sum(B, dims=2) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            A = [sv1, sv2, sv3]
+            B = @inferred tsample(Rational{T}, A, n_sim, num_cat(A))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+            @test all(==(3), sum(B, dims=(1,3)))
+            @test all(sum(B, dims=(2,3)) .≤ n_sim .* [3; 3; 2; 2; 1; 1])
+            A = sv3
+            B = @inferred tsample(Rational{T}, A, n_sim)
+            @test all(==(1), sum(B, dims=1))
+            A = sv2
+            tsample!(B, A)
+            @test all(==(2), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [2; 2; 2; 2; 1; 1])
+            A = sv1
+            tsample!(B, A)
+            @test all(==(3), sum(B, dims=1))
+            @test all(maximum(B, dims=2) .≤ [3; 3; 2; 2; 1; 1])
+            @test all(≥(0), minimum(B, dims=2))
+        end
+        # Real, AbstractFloat, Integer, Signed, Unsigned. work but should be avoided
+        A = [[sv1, sv2, SparseVector(6, [1,2,3,4,5,6], [0.5, 0.2, 0.2, 0.05,0.025, 0.025])]] # slight change to increase probability of Inexact throw
+        @test_throws InexactError tsample(Bool, A, 1000)
+        @test_throws MethodError tsample(Union{Int16, Int32}, A, n_sim)
+        B = Matrix{Union{Int16,Int32}}(undef, 6, 10)
+        @test_throws MethodError tsample!(B, A)
+    end
 end
