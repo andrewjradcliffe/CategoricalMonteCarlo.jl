d12df3ec533a1b81a0bcfbafb203d75122a96cc4
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-06-21 12:27:44 -0700

    Updates for vectorized Marsaglia sampler

--- src/vsampler.jl
+++ src/vsampler.jl
@@ -30,36 +30,38 @@
 
 # The bare minimum for `sample` interface-- covers all 4 other definitions.
 
-sample(::Type{S}, A, n_sim, n_cat; dims=:) where {S} = sample(S, A, n_sim, n_cat, dims)
-sample(::Type{S}, A, n_sim; dims=:) where {S} = sample(S, A, n_sim, num_cat(A), dims)
-sample(::Type{S}, A, n_sim::Int, n_cat::Int, dims::Int) where {S} = sample(S, A, n_sim, n_cat, (dims,))
+vsample(::Type{S}, A, n_sim, n_cat; dims=:) where {S} = vsample(S, A, n_sim, n_cat, dims)
+vsample(::Type{S}, A, n_sim; dims=:) where {S} = vsample(S, A, n_sim, num_cat(A), dims)
+vsample(::Type{S}, A, n_sim::Int, n_cat::Int, dims::Int) where {S} = vsample(S, A, n_sim, n_cat, (dims,))
 
-function sample(::Type{S}, A::AbstractArray{T, N}, n_sim::Int, n_cat::Int, dims::NTuple{P, Int}) where {S<:Real} where {P} where {T, N}
+function vsample(::Type{S}, A::AbstractArray{T, N}, n_sim::Int, n_cat::Int, dims::NTuple{P, Int}) where {S<:Real} where {P} where {T, N}
     Dᴬ = size(A)
     Dᴮ = tuple(n_cat, n_sim, ntuple(d -> d ∈ dims ? 1 : Dᴬ[d], Val(N))...)
     B = fill!(similar(A, S, Dᴮ), zero(S))
-    sample!(B, A)
+    vsample!(B, A)
 end
 
-function sample(::Type{S}, A::AbstractArray{T, N}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T, N}
+function vsample(::Type{S}, A::AbstractArray{T, N}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T, N}
     B = fill!(similar(A, S, (n_cat, n_sim)), zero(S))
-    sample!(B, A)
+    vsample!(B, A)
 end
 
 # The expected case: vectors of sparse vectors (as their bare components)
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
     U = Vector{Float64}(undef, size(B, 2))
-    Σω = Vector{T}()
+    K, V = Vector{Int}(), Vector{T}()
+    ix, q = Vector{Int}(), Vector{T}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         a = A[IA]
         for (Iₛ, ω) ∈ a
-            resize!(Σω, length(ω))
-            cumsum!(Σω, ω)
-            categorical!(C, U, Σω)
+            n = length(ω)
+            resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+            marsaglia!(K, V, q, ix, ω)
+            vmarsaglia_generate!(C, U, K, V)
             for j ∈ axes(B, 2)
                 c = C[j]
                 B[Iₛ[c], j, IR] += one(S)
@@ -70,18 +72,20 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Re
 end
 
 # A simplification: an array of sparse vectors
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Tuple{Vector{Int}, Vector{T}}, N}) where {S<:Real, N′} where {T<:AbstractFloat, N}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{Tuple{Vector{Int}, Vector{T}}, N}) where {S<:Real, N′} where {T<:AbstractFloat, N}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
     U = Vector{Float64}(undef, size(B, 2))
-    Σω = Vector{T}()
+    K, V = Vector{Int}(), Vector{T}()
+    ix, q = Vector{Int}(), Vector{T}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         Iₛ, ω = A[IA]
-        resize!(Σω, length(ω))
-        cumsum!(Σω, ω)
-        categorical!(C, U, Σω)
+        n = length(ω)
+        resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+        marsaglia!(K, V, q, ix, ω)
+        vmarsaglia_generate!(C, U, K, V)
         for j ∈ axes(B, 2)
             c = C[j]
             B[Iₛ[c], j, IR] += one(S)
@@ -91,28 +95,21 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Tuple{Vector{Int}, 
 end
 
 # The simplest case: a sparse vector
-sample(::Type{S}, A::Tuple{Vector{Int}, Vector{T}}, n_sim::Int, n_cat::Int, dims::Int) where {S<:Real} where {T<:AbstractFloat} = sample(S, A, n_sim, n_cat, :)
-sample(::Type{S}, A::Tuple{Vector{Int}, Vector{T}}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {T<:AbstractFloat} where {N} = sample(S, A, n_sim, n_cat, :)
+vsample(::Type{S}, A::Tuple{Vector{Int}, Vector{T}}, n_sim::Int, n_cat::Int, dims::Int) where {S<:Real} where {T<:AbstractFloat} = vsample(S, A, n_sim, n_cat, :)
+vsample(::Type{S}, A::Tuple{Vector{Int}, Vector{T}}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {T<:AbstractFloat} where {N} = vsample(S, A, n_sim, n_cat, :)
 
-function sample(::Type{S}, A::Tuple{Vector{Int}, Vector{T}}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T<:AbstractFloat} where {N}
+function vsample(::Type{S}, A::Tuple{Vector{Int}, Vector{T}}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T<:AbstractFloat} where {N}
     B = zeros(S, n_cat, n_sim)
-    sample!(B, A)
+    vsample!(B, A)
 end
 
-function sample!(B::AbstractMatrix{S}, A::Tuple{Vector{Int}, Vector{T}}) where {S<:Real} where {T<:AbstractFloat}
+function vsample!(B::AbstractMatrix{S}, A::Tuple{Vector{Int}, Vector{T}}) where {S<:Real} where {T<:AbstractFloat}
     _check_reducedims(B, A)
     Iₛ, ω = A
-    k = length(ω)
-    Σω = cumsum(ω)
-    s₀ = Σω[1]
+    K, V = marsaglia(ω)
+    C = vmarsaglia_generate!(Vector{Int}(undef, size(B, 2)), K, V)
     @inbounds for j ∈ axes(B, 2)
-        u = rand()
-        c = 1
-        s = s₀
-        while s < u && c < k
-            c += 1
-            s = Σω[c]
-        end
+        c = C[j]
         B[Iₛ[c], j] += one(S)
     end
     B
@@ -121,18 +118,27 @@ end
 ################
 # Specialized method for eltype(A)::Vector{Vector{Int}}
 # or, in other words, where the probability mass on each element is 1 / length(Iₛ)
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{Vector{Int}, M}, N} where {M}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{Vector{Int}, M}, N} where {M}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
+    U = Vector{Float64}(undef, size(B, 2))
+    K, V = Vector{Int}(), Vector{T}()
+    ix, q = Vector{Int}(), Vector{T}()
+    ω = Vector{Float64}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         a = A[IA]
         for Iₛ ∈ a
-            rand!(C, Iₛ)
+            n = length(Iₛ)
+            resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+            resize!(ω, n)
+            fill!(ω, inv(n))
+            marsaglia!(K, V, q, ix, ω)
+            vmarsaglia_generate!(C, U, K, V)
             for j ∈ axes(B, 2)
                 c = C[j]
-                B[c, j, IR] += one(S)
+                B[Iₛ[c], j, IR] += one(S)
             end
         end
     end
@@ -140,54 +146,68 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Re
 end
 
 # A simplification: an array of sparse vectors
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{Int}, N}) where {S<:Real, N′} where {N}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{Int}, N}) where {S<:Real, N′} where {N}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
+    U = Vector{Float64}(undef, size(B, 2))
+    K, V = Vector{Int}(), Vector{T}()
+    ix, q = Vector{Int}(), Vector{T}()
+    ω = Vector{Float64}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         Iₛ = A[IA]
-        rand!(C, Iₛ)
+        n = length(Iₛ)
+        resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+        resize!(ω, n)
+        fill!(ω, inv(n))
+        marsaglia!(K, V, q, ix, ω)
+        vmarsaglia_generate!(C, U, K, V)
         for j ∈ axes(B, 2)
             c = C[j]
-            B[c, j, IR] += one(S)
+            B[Iₛ[c], j, IR] += one(S)
         end
     end
     B
 end
 
 # The simplest case: a sparse vector
-function sample(::Type{S}, A::Vector{Int}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {N}
+function vsample(::Type{S}, A::Vector{Int}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {N}
     B = zeros(S, n_cat, n_sim)
-    sample!(B, A)
+    vsample!(B, A)
 end
 
-# Oddly, the fastest sampler is non-allocating -- most likely due to
+# Oddly, the fastest vsampler is non-allocating -- most likely due to
 # the elimination of store + access instructions associated with using a temporary array.
-function sample!(B::AbstractMatrix{S}, A::Vector{Int}) where {S<:Real}
+function vsample!(B::AbstractMatrix{S}, A::Vector{Int}) where {S<:Real}
     _check_reducedims(B, A)
+    n = length(A)
+    K, V = marsaglia(fill(inv(n), n))
+    C = vmarsaglia_generate!(Vector{Int}(undef, size(B, 2)), K, V)
     @inbounds for j ∈ axes(B, 2)
-        c = rand(A)
-        B[c, j] += one(S)
+        c = C[j]
+        B[Iₛ[c], j] += one(S)
     end
     B
 end
 
 ################
 # General case: dense vectors, the linear index of which indicates the category
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
     U = Vector{Float64}(undef, size(B, 2))
-    Σω = Vector{T}()
+    K, V = Vector{Int}(), Vector{T}()
+    ix, q = Vector{Int}(), Vector{T}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         a = A[IA]
         for ω ∈ a
-            resize!(Σω, length(ω))
-            cumsum!(Σω, ω)
-            categorical!(C, U, Σω)
+            n = length(ω)
+            resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+            marsaglia!(K, V, q, ix, ω)
+            vmarsaglia_generate!(C, U, K, V)
             for j ∈ axes(B, 2)
                 c = C[j]
                 B[c, j, IR] += one(S)
@@ -198,18 +218,20 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Re
 end
 
 # A simplification: an array of dense vectors
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{T}, N}) where {S<:Real, N′} where {T<:AbstractFloat, N}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{T}, N}) where {S<:Real, N′} where {T<:AbstractFloat, N}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
     U = Vector{Float64}(undef, size(B, 2))
-    Σω = Vector{T}()
+    K, V = Vector{Int}(), Vector{T}()
+    ix, q = Vector{Int}(), Vector{T}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         ω = A[IA]
-        resize!(Σω, length(ω))
-        cumsum!(Σω, ω)
-        categorical!(C, U, Σω)
+        n = length(ω)
+        resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+        marsaglia!(K, V, q, ix, ω)
+        vmarsaglia_generate!(C, U, K, V)
         for j ∈ axes(B, 2)
             c = C[j]
             B[c, j, IR] += one(S)
@@ -219,29 +241,22 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{Vector{T}, N}) wher
 end
 
 # The simplest case: a dense vector
-sample(::Type{S}, A::Vector{T}, n_sim::Int, n_cat::Int, dims::Int) where {S<:Real} where {T<:AbstractFloat} = sample(S, A, n_sim, n_cat, :)
-sample(::Type{S}, A::Vector{T}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {T<:AbstractFloat} where {N} = sample(S, A, n_sim, n_cat, :)
+vsample(::Type{S}, A::Vector{T}, n_sim::Int, n_cat::Int, dims::Int) where {S<:Real} where {T<:AbstractFloat} = vsample(S, A, n_sim, n_cat, :)
+vsample(::Type{S}, A::Vector{T}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {T<:AbstractFloat} where {N} = vsample(S, A, n_sim, n_cat, :)
 
-function sample(::Type{S}, A::Vector{T}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T<:AbstractFloat} where {N}
+function vsample(::Type{S}, A::Vector{T}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T<:AbstractFloat} where {N}
     B = zeros(S, n_cat, n_sim)
-    sample!(B, A)
+    vsample!(B, A)
 end
 
-function sample!(B::AbstractMatrix{S}, A::Vector{T}) where {S<:Real} where {T<:AbstractFloat}
+function vsample!(B::AbstractMatrix{S}, A::Vector{T}) where {S<:Real} where {T<:AbstractFloat}
     _check_reducedims(B, A)
     ω = A
-    k = length(ω)
-    Σω = cumsum(ω)
-    s₀ = Σω[1]
+    K, V = marsaglia(ω)
+    C = vmarsaglia_generate!(Vector{Int}(undef, size(B, 2)), K, V)
     @inbounds for j ∈ axes(B, 2)
-        u = rand()
-        c = 1
-        s = s₀
-        while s < u && c < k
-            c += 1
-            s = Σω[c]
-        end
-        B[c, j] += one(S)
+        c = C[j]
+        B[c, j]+= one(S)
     end
     B
 end
@@ -249,21 +264,23 @@ end
 
 ################
 # General case: sparse vectors, the nzval of which indicates the category
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Real, N′} where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
     U = Vector{Float64}(undef, size(B, 2))
-    Σω = Vector{Tv}()
+    K, V = Vector{Int}(), Vector{Tv}()
+    ix, q = Vector{Int}(), Vector{Tv}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         a = A[IA]
         for sv ∈ a
             (; n, nzind, nzval) = sv
             Iₛ, ω = nzind, nzval
-            resize!(Σω, length(ω))
-            cumsum!(Σω, ω)
-            categorical!(C, U, Σω)
+            n = length(ω)
+            resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+            marsaglia!(K, V, q, ix, ω)
+            vmarsaglia_generate!(C, U, K, V)
             for j ∈ axes(B, 2)
                 c = C[j]
                 B[Iₛ[c], j, IR] += one(S)
@@ -274,20 +291,22 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{R, N}) where {S<:Re
 end
 
 # A simplification: an array of sparse vectors
-function sample!(B::AbstractArray{S, N′}, A::AbstractArray{SparseVector{Tv, Ti}, N}) where {S<:Real, N′} where {Tv<:AbstractFloat, Ti<:Integer, N}
+function vsample!(B::AbstractArray{S, N′}, A::AbstractArray{SparseVector{Tv, Ti}, N}) where {S<:Real, N′} where {Tv<:AbstractFloat, Ti<:Integer, N}
     _check_reducedims(B, A)
     keep, default = Broadcast.shapeindexer(axes(B)[3:end])
     C = Vector{Int}(undef, size(B, 2))
     U = Vector{Float64}(undef, size(B, 2))
-    Σω = Vector{Tv}()
+    K, V = Vector{Int}(), Vector{Tv}()
+    ix, q = Vector{Int}(), Vector{Tv}()
     @inbounds for IA ∈ CartesianIndices(A)
         IR = Broadcast.newindex(IA, keep, default)
         sv = A[IA]
         (; n, nzind, nzval) = sv
         Iₛ, ω = nzind, nzval
-        resize!(Σω, length(ω))
-        cumsum!(Σω, ω)
-        categorical!(C, U, Σω)
+        n = length(ω)
+        resize!(K, n); resize!(V, n); resize!(ix, n); resize!(q, n)
+        marsaglia!(K, V, q, ix, ω)
+        vmarsaglia_generate!(C, U, K, V)
         for j ∈ axes(B, 2)
             c = C[j]
             B[Iₛ[c], j, IR] += one(S)
@@ -297,29 +316,22 @@ function sample!(B::AbstractArray{S, N′}, A::AbstractArray{SparseVector{Tv, Ti
 end
 
 # The simplest case: a sparse vector
-sample(::Type{S}, A::SparseVector{T}, n_sim::Int, n_cat::Int, dims::Int) where {S<:Real} where {T<:AbstractFloat} = sample(S, A, n_sim, n_cat, :)
-sample(::Type{S}, A::SparseVector{T}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {T<:AbstractFloat} where {N} = sample(S, A, n_sim, n_cat, :)
+vsample(::Type{S}, A::SparseVector{T}, n_sim::Int, n_cat::Int, dims::Int) where {S<:Real} where {T<:AbstractFloat} = vsample(S, A, n_sim, n_cat, :)
+vsample(::Type{S}, A::SparseVector{T}, n_sim::Int, n_cat::Int, dims::NTuple{N, Int}) where {S<:Real} where {T<:AbstractFloat} where {N} = vsample(S, A, n_sim, n_cat, :)
 
-function sample(::Type{S}, A::SparseVector{T}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T<:AbstractFloat} where {N}
+function vsample(::Type{S}, A::SparseVector{T}, n_sim::Int, n_cat::Int, ::Colon) where {S<:Real} where {T<:AbstractFloat} where {N}
     B = zeros(S, n_cat, n_sim)
-    sample!(B, A)
+    vsample!(B, A)
 end
 
-function sample!(B::AbstractMatrix{S}, A::SparseVector{T}) where {S<:Real} where {T<:AbstractFloat}
+function vsample!(B::AbstractMatrix{S}, A::SparseVector{T}) where {S<:Real} where {T<:AbstractFloat}
     _check_reducedims(B, A)
     (; n, nzind, nzval) = A
     Iₛ, ω = nzind, nzval
-    k = length(ω)
-    Σω = cumsum(ω)
-    s₀ = Σω[1]
+    K, V = marsaglia(ω)
+    C = vmarsaglia_generate!(Vector{Int}(undef, size(B, 2)), K, V)
     @inbounds for j ∈ axes(B, 2)
-        u = rand()
-        c = 1
-        s = s₀
-        while s < u && c < k
-            c += 1
-            s = Σω[c]
-        end
+        c = C[j]
         B[Iₛ[c], j] += one(S)
     end
     B
