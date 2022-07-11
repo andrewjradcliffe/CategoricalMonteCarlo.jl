b8e790601b73435e9bba44fae02df0f46eb478ef
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-07-11 14:16:29 -0700

    Keep package definition reasonably up-to-date

--- src/CategoricalMonteCarlo.jl
+++ src/CategoricalMonteCarlo.jl
@@ -1,12 +1,21 @@
 module CategoricalMonteCarlo
 
 using Random, Test, BenchmarkTools
+using SparseArrays
+using LoopVectorization
+using Polyester
+# using VectorizedReduction
+using MarsagliaDiscreteSamplers
+
+import MarsagliaDiscreteSamplers: _sqhist_init
 
 export sample, sample!, tsample, tsample!, num_cat, categorical, categorical!
 
 include("utils.jl")
-include("bernoulliprocess.jl")
+include("normalizations.jl")
 include("sampler.jl")
-include("tsampler.jl")
+include("tsampler_batch.jl")
+include("vsampler.jl")
+include("vtsampler_batch.jl")
 
 end
