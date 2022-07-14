07469f87237e0fe064a3fc7d7d78ef4eefc70bdb
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-07-14 15:05:27 -0700

    Improve type-stability; fix `bounds_cat`
    A deeply nested type instability in `num_cat` was revealed
    when it became apparent that `bounds_cat` based on extrema
    was not the desired result, and, moreover, that separate
    minimum/maximum searches were of approximate speed as extrema
    was handling tuples and the former scalars.
    These changes, while superficial, fix the type instability

--- src/utils.jl
+++ src/utils.jl
@@ -5,56 +5,92 @@
 #
 ############################################################################################
 # Conveniences
-_maximum_maybe(x::AbstractVector{T}) where {T<:Real} = isempty(x) ? zero(T) : maximum(x)
+_maximum_maybe(x::AbstractVector{T}) where {T<:Integer} = isempty(x) ? zero(T) : maximum(x)
+_maximum_maybe((x, y)::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _maximum_maybe(x)
 
-num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M} = maximum(a -> maximum(_maximum_maybe, a, init=0), A, init=0)
-num_cat(A::AbstractArray{T, N}) where {T<:Tuple{Vector{Int}, Vector{<:AbstractFloat}}, N} =
-    maximum(num_cat, A, init=0)
-num_cat(A::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _maximum_maybe(A[1])
+num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M} =
+    isempty(A) ? 0 : maximum(num_cat, A, init=0)
+num_cat(A::AbstractArray{Tuple{Vector{Int}, Vector{T}}, N}) where {T<:AbstractFloat, N} =
+    isempty(A) ? 0 : maximum(_maximum_maybe, A, init=0)
+num_cat(A::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _maximum_maybe(A)
 
 num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{Int}, M}, N} where {M} =
-    maximum(a -> maximum(_maximum_maybe, a, init=0), A, init=0)
-num_cat(A::AbstractArray{Vector{Int}, N}) where {N} = maximum(_maximum_maybe, A, init=0)
+    isempty(A) ? 0 : maximum(num_cat, A, init=0)
+num_cat(A::AbstractArray{Vector{Int}, N}) where {N} =
+    isempty(A) ? 0 : maximum(_maximum_maybe, A, init=0)
 num_cat(A::Vector{Int}) = _maximum_maybe(A)
 
-num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M} = maximum(a -> maximum(_maximum_maybe, a, init=0), A, init=0)
-num_cat(A::AbstractArray{Vector{T}, N}) where {T<:AbstractFloat, N} = maximum(length, A, init=0)
+num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M} =
+    isempty(A) ? 0 : maximum(num_cat, A, init=0)
+num_cat(A::AbstractArray{Vector{T}, N}) where {T<:AbstractFloat, N} =
+    isempty(A) ? 0 : maximum(length, A, init=0)
 num_cat(A::Vector{T}) where {T<:AbstractFloat} = length(A)
 
-num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M} = maximum(a -> maximum(_maximum_maybe, a, init=0), A, init=0)
+num_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M} =
+    isempty(A) ? 0 : maximum(num_cat, A, init=0)
 num_cat(A::AbstractArray{SparseVector{Tv, Ti}, N}) where {Tv<:AbstractFloat, Ti<:Integer, N} =
-    maximum(length, A, init=0)
-num_cat(A::SparseVector{T}) where {T<:AbstractFloat} = length(A)
+    isempty(A) ? 0 : maximum(length, A, init=0)
+num_cat(A::SparseVector{Tv, Ti}) where {Tv<:AbstractFloat, Ti<:Integer} = length(A)
+
+_minimum_maybe(x::AbstractVector{T}) where {T<:Integer} = isempty(x) ? one(T) : minimum(x)
+_minimum_maybe((x, y)::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _minimum_maybe(x)
+
+num_cat_min(A::AbstractArray{R, N}) where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M} =
+    isempty(A) ? 1 : minimum(num_cat_min, A, init=1)
+num_cat_min(A::AbstractArray{Tuple{Vector{Int}, Vector{T}}, N}) where {T<:AbstractFloat, N} =
+    isempty(A) ? 1 : minimum(_minimum_maybe, A, init=1)
+num_cat_min(A::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _minimum_maybe(A)
+
+num_cat_min(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{Int}, M}, N} where {M} =
+    isempty(A) ? 1 : minimum(num_cat_min, A, init=1)
+num_cat_min(A::AbstractArray{Vector{Int}, N}) where {N} =
+    isempty(A) ? 1 : minimum(_minimum_maybe, A, init=1)
+num_cat_min(A::Vector{Int}) = _minimum_maybe(A)
+
+num_cat_min(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M} =
+    isempty(A) ? 1 : minimum(num_cat_min, A, init=1)
+num_cat_min(A::AbstractArray{Vector{T}, N}) where {T<:AbstractFloat, N} =
+    isempty(A) ? 1 : minimum(length, A, init=1)
+num_cat_min(A::Vector{T}) where {T<:AbstractFloat} = length(A)
+
+num_cat_min(A::AbstractArray{R, N}) where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M} =
+    isempty(A) ? 1 : minimum(num_cat_min, A, init=1)
+num_cat_min(A::AbstractArray{SparseVector{Tv, Ti}, N}) where {Tv<:AbstractFloat, Ti<:Integer, N} =
+    isempty(A) ? 1 : minimum(length, A, init=1)
+num_cat_min(A::SparseVector{Tv, Ti}) where {Tv<:AbstractFloat, Ti<:Integer} = length(A)
 
 ####
-_extrema_maybe(x::AbstractVector{T}) where {T<:Real} = isempty(x) ? (zero(T), zero(T)) : extrema(x)
+# _extrema_maybe(x::AbstractVector{T}) where {T<:Real} = isempty(x) ? (zero(T), zero(T)) : extrema(x)
 
-bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M} =
-    (((lb1, ub1), (lb2, ub2)) = extrema(bounds_cat, A, init=((1,1), (0,0)));
-     extrema((lb1, ub1, lb2, ub2)))
-bounds_cat(A::AbstractArray{T, N}) where {T<:Tuple{Vector{Int}, Vector{<:AbstractFloat}}, N} =
-    (((lb1, ub1), (lb2, ub2)) = extrema(bounds_cat, A, init=((1,1), (0,0)));
-     extrema((lb1, ub1, lb2, ub2)))
-bounds_cat(A::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _extrema_maybe(A[1])
+# bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Tuple{Vector{Int}, Vector{T}}, M}, N} where {T<:AbstractFloat, M} =
+#     (((lb1, ub1), (lb2, ub2)) = extrema(bounds_cat, A, init=((1,1), (0,0)));
+#      extrema((lb1, ub1, lb2, ub2)))
+# bounds_cat(A::AbstractArray{T, N}) where {T<:Tuple{Vector{Int}, Vector{<:AbstractFloat}}, N} =
+#     (((lb1, ub1), (lb2, ub2)) = extrema(bounds_cat, A, init=((1,1), (0,0)));
+#      extrema((lb1, ub1, lb2, ub2)))
+# bounds_cat(A::Tuple{Vector{Int}, Vector{T}}) where {T<:AbstractFloat} = _extrema_maybe(A[1])
 
-bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{Int}, M}, N} where {M} =
-    (((lb1, ub1), (lb2, ub2)) = extrema(bounds_cat, A, init=((1,1), (0,0)));
-     extrema((lb1, ub1, lb2, ub2)))
-bounds_cat(A::AbstractArray{Vector{Int}, N}) where {N} =
-    (((lb1, ub1), (lb2, ub2)) = extrema(_extrema_maybe, A, init=((1,1), (0,0)));
-     extrema((lb1, ub1, lb2, ub2)))
-bounds_cat(A::Vector{Int}) = _extrema_maybe(A)
+# bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{Int}, M}, N} where {M} =
+#     (((lb1, ub1), (lb2, ub2)) = extrema(bounds_cat, A, init=((1,1), (0,0)));
+#      extrema((lb1, ub1, lb2, ub2)))
+# bounds_cat(A::AbstractArray{Vector{Int}, N}) where {N} =
+#     (((lb1, ub1), (lb2, ub2)) = extrema(_extrema_maybe, A, init=((1,1), (0,0)));
+#      extrema((lb1, ub1, lb2, ub2)))
+# bounds_cat(A::Vector{Int}) = _extrema_maybe(A)
 
-bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
-bounds_cat(A::AbstractArray{Vector{T}, N}) where {T<:AbstractFloat, N} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
-bounds_cat(A::Vector{T}) where {T<:AbstractFloat} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
+# bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{Vector{T}, M}, N} where {T<:AbstractFloat, M} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
+# bounds_cat(A::AbstractArray{Vector{T}, N}) where {T<:AbstractFloat, N} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
+# bounds_cat(A::Vector{T}) where {T<:AbstractFloat} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
 
-bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M} =
-    (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
-bounds_cat(A::AbstractArray{SparseVector{Tv, Ti}, N}) where {Tv<:AbstractFloat, Ti<:Integer, N} =
-    (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
-bounds_cat(A::SparseVector{T}) where {T<:AbstractFloat} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
+# bounds_cat(A::AbstractArray{R, N}) where {R<:AbstractArray{SparseVector{Tv, Ti}, M}, N} where {Tv<:AbstractFloat, Ti<:Integer, M} =
+#     (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
+# bounds_cat(A::AbstractArray{SparseVector{Tv, Ti}, N}) where {Tv<:AbstractFloat, Ti<:Integer, N} =
+#     (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
+# bounds_cat(A::SparseVector{T}) where {T<:AbstractFloat} = (n = num_cat(A); n ≥ 1 ? (1, n) : (0, 0))
 
+#
+bounds_cat(x::Int, y::Int) = (x, y)
+bounds_cat(A::T) where {T} = bounds_cat(num_cat_min(A), num_cat(A))
 ################
 function _checkindex_reducedims(ax, lb, ub)
     (checkindex(Bool, ax, lb) && checkindex(Bool, ax, ub)) || throw(DimensionMismatch("cannot sample from categories on range $(lb:ub) into array with first dimension $(ax)"))
